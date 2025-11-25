4712a1d1817dc51a298d01900a970eb0ddd9b69f
KovalyovaMaria <idKovalyova@gmail.com>
2025-11-25 15:24:49 +0200

    Fix project structure

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -1,10 +1,17 @@
-import path from 'path';
-import express from 'express';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
