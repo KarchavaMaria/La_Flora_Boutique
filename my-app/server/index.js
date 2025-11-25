b0ad852af7d22f97796ae2cb9c1f703abb74301e
KovalyovaMaria <idKovalyova@gmail.com>
2025-11-25 16:03:33 +0200

    Edit app

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -9,6 +9,6 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("/.*/", (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
