4679cde443b7c7a19cb052113c8c97f6e11200cd
KovalyovaMaria <idKovalyova@gmail.com>
2025-11-25 15:31:36 +0200

    Fix project structure

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -12,6 +12,3 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
