674df2efee110a1b107c5968ecb89a6713999846
KovalyovaMaria <idKovalyova@gmail.com>
2025-11-25 15:00:31 +0200

    Fix project structure

---
+++ my-app/server/index.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
