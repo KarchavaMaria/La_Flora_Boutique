e6e8e431bcd389bbb28087b23a5e9d2c606c758a
KovalyovaMaria <idKovalyova@gmail.com>
2025-11-25 16:10:10 +0200

    Edit app

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-
-const app = express();
+import app from './app.js';
+import config from 'config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,4 +11,10 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
+const PORT = config.get('PORT');
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
